3fa3c80206fa601200a6c0d9bb78227c6415c15e
N4m <nam@thelinuxplace.com>
2016-07-05 11:44:01 -0700

    Create role.upgrader.js

---
+++ role.upgrader.js
@@ -0,0 +1,43 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.upgrader');
+ * mod.thing == 'a thing'; // true
+ */
+
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if (typeof creep.memory.source === "undefined") {
+            creep.memory.source = Math.floor(Math.random()*sources.length);
+            console.log('Setting creep ('+creep.name+') source to '+creep.memory.source);
+        // } else {
+        //     console.log('Creep already set to '+ creep.memory.source);
+        }
+
+        if(creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+	    }
+	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.upgrading = true;
+	    }
+
+	    if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[creep.memory.source]);
+            }
+        }
+	}
+};
+
+module.exports = roleUpgrader;
