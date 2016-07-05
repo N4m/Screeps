23dad4f8dcbbffe7f96b4b97d125138b365fce00
N4m <nam@thelinuxplace.com>
2016-07-05 11:41:23 -0700

    Create role.builder.js

---
+++ role.builder.js
@@ -0,0 +1,66 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.builder');
+ * mod.thing == 'a thing'; // true
+ */
+
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if (typeof creep.memory.source === "undefined") {
+            creep.memory.source = Math.floor(Math.random()*sources.length);
+            console.log('Setting creep ('+creep.name+') source to '+creep.memory.source);
+        // } else {
+        //     console.log('Creep already set to '+ creep.memory.source);
+        }
+
+	    if(creep.memory.building && creep.carry.energy == 0) {
+            creep.memory.building = false;
+	    }
+	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.building = true;
+	    }
+
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            } else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.hits < structure.hitsMax;
+                    }
+                });
+                if(targets.length > 0) {
+                    if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0]);
+                    }
+                }
+            }
+	    } else {
+	        var containers = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] !== 0;
+                }
+            });
+            if(containers.length > 0) {
+                if(containers[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(containers[0]);
+                }
+            } else {
+                if(creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[creep.memory.source]);
+                }
+            }
+	    }
+	}
+};
+
+module.exports = roleBuilder;
