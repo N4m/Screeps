b24d22ea14587dc1f49a79de133c3b761090ab11
Colton McInroy <colton@dosarrest.com>
2016-07-05 17:15:31 -0700

    Fixing creep room report

--- roommanager.js
+++ roommanager.js
@@ -23,33 +23,39 @@ var roommanager = {
                     'Level: '+room.controller.level+', ',
                     'Upgrade Progress: '+room.controller.progress+'/'+room.controller.progressTotal
                 );
-                var roomcreeps = [];
-                for (var c in Game.creeps) {
-                    if (Game.creeps[c].room == room) {
-                        roomcreeps.push(Game.creeps[c]);
-                    }
+                var creepReport = [];
+                for (var role in roles) {
+                    roles[role].creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role && creep.room == room);
+                    creepReport.push(role+'='+roles[role].creeps.length+'/'+roles[role].amount);
                 }
-                var role;
-                for (var rc in roomcreeps) {
-                    var creep = roomcreeps[rc];
-                    for (role in roles) {
-                        if (creep.memory.role == role) {
-                            if (roles[role].count) {
-                                roles[role].count++;
-                            } else {
-                                roles[role].count = 1;
-                            }
-                            break;
-                        }
-                    }
-                }
-                var creepStatus = [];
-                for (role in roles) {
-                    if (roles[role].count) {
-                        creepStatus.push(role+':'+roles[role].count+'/'+roles[role].ammount);
-                    }
-                }
-                console.log('ROOMMANAGER: Room Creep '+room.name+' Status Report: '+creepStatus.join(', '));
+                console.log('ROOMMANAGER: Room Creep '+room.name+' Status Report: '+creepReport.join(', '));
+                // var roomcreeps = [];
+                // for (var c in Game.creeps) {
+                //     if (Game.creeps[c].room == room) {
+                //         roomcreeps.push(Game.creeps[c]);
+                //     }
+                // }
+                // var role;
+                // for (var rc in roomcreeps) {
+                //     var creep = roomcreeps[rc];
+                //     for (role in roles) {
+                //         if (creep.memory.role == role) {
+                //             if (roles[role].count) {
+                //                 roles[role].count++;
+                //             } else {
+                //                 roles[role].count = 1;
+                //             }
+                //             break;
+                //         }
+                //     }
+                // }
+                // var creepStatus = [];
+                // for (role in roles) {
+                //     if (roles[role].count) {
+                //         creepStatus.push(role+':'+roles[role].count+'/'+roles[role].ammount);
+                //     }
+                // }
+                // console.log('ROOMMANAGER: Room Creep '+room.name+' Status Report: '+creepStatus.join(', '));
             }
         }
     }
